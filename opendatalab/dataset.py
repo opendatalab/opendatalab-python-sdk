c2f8858225dc5bb24fc5ae8ede43a7c55f169c6b
Zhang Chaobin <zhangchaobin@sensetime.com>
2022-04-06 15:33:36 +0800

    Implement dataset/get method

--- opendatalab/dataset.py
+++ opendatalab/dataset.py
@@ -1,7 +1,9 @@
 import oss2
+import requests
 
 from urllib.parse import urlparse
 from .api import OpenDataLabAPI
+from requests.adapters import HTTPAdapter
 
 
 def parse_url(url: str) -> (str, str):
@@ -15,17 +17,46 @@ def parse_url(url: str) -> (str, str):
 
 
 class Dataset(object):
-    def __init__(self, url: str, token: str = "") -> None:
+    def __init__(
+        self, url: str, token: str = "", storage_format: str = "source"
+    ) -> None:
         host, dataset_id = parse_url(url)
         self.dataset_id = dataset_id
         self.open_data_lab_api = OpenDataLabAPI(host, token)
+        self.storage_format = storage_format
+
+        self.oss_bucket = None
+        self.oss_path_prefix = ""
+        self.init_oss_bucket()
 
     def get(self, key: str):
-        sts = self.open_data_lab_api.get_dataset_sts(self.dataset_id)
+        object_key = f"{self.oss_path_prefix}/{self.storage_format}/" + key
+        try:
+            return self.oss_bucket.get_object(object_key)
+        except oss2.exceptions.ServerError as e:
+            if "InvalidAccessKeyId" not in str(e):
+                raise e
 
+            self.init_oss_bucket()
+            return self.oss_bucket.get_object(object_key)
+
+    def init_oss_bucket(self):
+        sts = self.open_data_lab_api.get_dataset_sts(self.dataset_id)
         auth = oss2.StsAuth(
             sts["accessKeyId"], sts["accessKeySecret"], sts["securityToken"]
         )
-        bucket_name = sts["path"].replace("oss://", "").split("/")[0]
-        bucket = oss2.Bucket(auth, sts["endpoint"]["internet"], bucket_name)
-        return bucket.get_object("MNIST/source/train-labels-idx1-ubyte")
+        path_info = sts["path"].replace("oss://", "").split("/")
+        bucket_name = path_info[0]
+        self.oss_bucket = oss2.Bucket(auth, self.select_endpoint(sts), bucket_name)
+        self.oss_path_prefix = "/".join(path_info[1:])
+
+    @classmethod
+    def select_endpoint(cls, sts):
+        try:
+            s = requests.Session()
+            vpc_endpoint = sts["endpoint"]["vpc"]
+            s.mount(vpc_endpoint, HTTPAdapter(max_retries=0))
+            s.get(vpc_endpoint, timeout=(0.5, 1))
+            return vpc_endpoint
+        except requests.exceptions.ConnectTimeout or requests.exceptions.ReadTimeout:
+            return sts["endpoint"]["internet"]
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,5 +5,28 @@ import opendatalab
 
 def test_dataset():
     dataset = opendatalab.Dataset(url="http://opendatalab-test3.shlab.tech/datasets/4")
-    print(dataset.get(""))
+    print(dataset.get("t10k-images-idx3-ubyte"))
     print("ok")
+
+
+def test_select_endpoint():
+    dataset = opendatalab.Dataset(url="http://opendatalab-test3.shlab.tech/datasets/4")
+    endpoint = dataset.select_endpoint(
+        {
+            "endpoint": {
+                "vpc": "https://oss-cn-shanghai.aliyuncs.com",
+                "internet": "https://foo-cn-shanghai.foo.com/",
+            }
+        }
+    )
+    assert endpoint == "https://oss-cn-shanghai.aliyuncs.com"
+
+    endpoint = dataset.select_endpoint(
+        {
+            "endpoint": {
+                "vpc": "https://oss-cn-shanghai-internal.aliyuncs.com",
+                "internet": "https://oss-cn-shanghai.aliyuncs.com",
+            }
+        }
+    )
+    assert endpoint == "https://oss-cn-shanghai.aliyuncs.com"
