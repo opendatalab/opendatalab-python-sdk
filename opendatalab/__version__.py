ffe5d1049bd03b1aed69857a33ba1ea076d8e92f
weixingjian <weixingjian@pjlab.org.cn>
2023-04-28 15:50:33 +0800

    for product

--- opendatalab/__version__.py
+++ opendatalab/__version__.py
@@ -7,13 +7,9 @@
 
 """OpenDataLab python SDK version info."""
 
-__url__ = 'https://opendatalab-ut.shlab.tech'
+__url__ = "https://opendatalab.org.cn"
 __version__ = "0.0.6"
 __svc__ = '2.0'
-#odl_clientId = "kmz3bkwzlaa3wrq8pvwa"
-# odl_clientId = "qja9jy5wnjyqwvylmeqw"
-#uaa_url_prefix = "https://sso.openxlab.org.cn/gw/uaa-be"
-uaa_url_prefix = "https://sso.staging.openxlab.org.cn/gw/uaa-be"
-# baseUrl: https://sso.staging.openxlab.org.cn/gw/uaa-be
-odl_clientId= 'ypkl8bwo0eb5ao1b96no'
+odl_clientId = "kmz3bkwzlaa3wrq8pvwa"
+uaa_url_prefix = "https://sso.openxlab.org.cn/gw/uaa-be"
 # clientSecret: 97gdrvwwzob86q2rneq2x95w6bnxkpqj5oak1ype
